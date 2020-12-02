470ff484c7cc3b7a2c8495306ee8370611af1f50
Francesco Fico <fficusbenjamin@gmail.com>
2020-12-02 20:51:16 +0000

    Update Board.java

--- src/time_ontology/Board.java
+++ src/time_ontology/Board.java
@@ -4,21 +4,25 @@ import java.util.ArrayList;
 import java.util.List;
 import jade.content.Predicate;
 
+///------------------------------------------------------------------------
+///   Class:		Board (Class)
+///   Description:	Class that , being a predicate of the ontology,holds
+///					the unwanted tutorials to be swapped on a board.
+///
+///
+///
+///   Author:		Francesco Fico (40404272)     Date: 02/12/2020
+///------------------------------------------------------------------------
+
 
 public class Board implements Predicate {
-
+	//creates a new list of tutorials for the board
 	private List<Tutorial> board = new ArrayList<>();
-	
+	//get and set for the board
 	public List<Tutorial> getBoard() {
 		return board;
 	}
-
 	public void setBoard(List<Tutorial> board) {
 		this.board = board;
 	}
-
-
-
-
-	
 }
