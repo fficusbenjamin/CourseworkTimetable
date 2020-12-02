654e2f1ff4cee1421a1fdeb654b83f7655871a7e
Francesco Fico <fficusbenjamin@gmail.com>
2020-12-02 21:39:18 +0000

    Update TimeOntology.java

--- src/time_ontology/TimeOntology.java
+++ src/time_ontology/TimeOntology.java
@@ -4,21 +4,33 @@ import jade.content.onto.BeanOntology;
 import jade.content.onto.BeanOntologyException;
 import jade.content.onto.Ontology;
 
+import java.io.Serial;
+
+///------------------------------------------------------------------------
+///   Class:		TimeOntology (Class)
+///   Description:	Tutorial class that states all its properties
+///
+///
+///
+///
+///   Author:		Francesco Fico (40404272)     Date: 02/12/2020
+///------------------------------------------------------------------------
+
 public class TimeOntology extends BeanOntology {
-	/**
-	 * 
-	 */
+
+	@Serial
 	private static final long serialVersionUID = 1L;
-	private static Ontology theInstance = new TimeOntology("my_ontology");
-	
+	//initialisation of the ontology instance
+	private static final Ontology theInstance = new TimeOntology("ontology");
+	//getter for the ontology instance
 	public static Ontology getInstance(){
 		return theInstance;
 	}
-	//singleton pattern
+	//singleton pattern for the TimeOntology instance
 	private TimeOntology(String name) {
 		super(name);
 		try {
-			
+			//add the tutorial and all the predicates to the ontology instance
 			add(Tutorial.class);
 			add(Slot.class);
 			add(Board.class);
