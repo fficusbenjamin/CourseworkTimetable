676374640fdf9b10761e06abaafe5e54a5ac4101
Francesco Fico <fficusbenjamin@gmail.com>
2020-12-02 21:19:43 +0000

    Update Slot.java

--- src/time_ontology/Slot.java
+++ src/time_ontology/Slot.java
@@ -3,12 +3,26 @@ package time_ontology;
 import jade.content.Predicate;
 import jade.core.AID;
 
+import java.io.Serial;
+
+///------------------------------------------------------------------------
+///   Class:		Slot (Class)
+///   Description:	Class that , being a predicate of the ontology, holds
+///					the actual tutorial to be stored on the board.
+///
+///
+///
+///   Author:		Francesco Fico (40404272)     Date: 02/12/2020
+///------------------------------------------------------------------------
+
 public class Slot implements Predicate {
 
+	@Serial
 	private static final long serialVersionUID = 1L;
+	//initialise the class variables
 	private AID slotOwner;
 	private Tutorial tutorial;
-	
+	//getters and setters for the variables
 	public AID getSlotOwner() {
 		return slotOwner;
 	}
